8a4c88bb031d8fcdde3ef061c8af292c5dd3bb8d
David Janes <davidjanes2@hotmail.com>
2021-01-21 21:05:06 +0100

    Amazon S3

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'blog',
     'rating',
     'feedback',
+
+    'storages',
     
     'django.contrib.admin',
     'django.contrib.auth',
@@ -144,3 +146,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE = "virtual"
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
